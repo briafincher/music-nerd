ed6ccb4de023c4f0a41bfe95261674df0132504e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-09 17:51:54 -0700

    Created genres json

--- genre_nodes.py
+++ genre_nodes.py
@@ -1,13 +1,26 @@
+import json
+from pprint import pprint
+
+
 nodes = []
 
 links = []
 
 for node in open('all_genre_nodes.txt'):
-    node.rstrip()
+    node.strip()
     if node not in nodes:
-        links.append(node)
+        nodes.append(node)
 
 for link in open('all_genre_links.txt'):
-    link.rstrip()
+    link.strip()
     links.append(link)
 
+genres = json.dumps({'nodes': nodes,
+                     'links': links})
+
+pprint(genres)
+
+# print "{"
+# print "'nodes': {},".format(nodes)
+# print "'links': {}".format(links)
+# print "}"
