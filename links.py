3f23a15f59fe9cff900427a5540b8a3c86622279
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-16 16:30:51 -0700

    Script to filter genre map

--- links.py
+++ links.py
@@ -1,6 +1,7 @@
 from model2 import app, connect_to_db
-from model2 import Genre, RelatedGenres
+from model2 import Genre, RelatedGenres, GenreAverages
 import json
+import pdb
 
 
 def add_nodes(relationships):
@@ -11,15 +12,10 @@ def add_nodes(relationships):
         genre1 = Genre.query.filter_by(genre_id=relationship.genre1_id).first()
         genre2 = Genre.query.filter_by(genre_id=relationship.genre2_id).first()
 
-        if genre1.name not in nodes:
-            nodes.append({'id': genre1.name, 'group': 1})
-        if genre2.name not in nodes:
-            nodes.append({'id': genre2.name, 'group': 1})
+        nodes.append({"id": genre1.name, "group": 1})
+        nodes.append({"id": genre2.name, "group": 1})
 
-    # for node in nodes:
-    #     print node
-
-    return nodes
+    return [dict(genre) for genre in set([tuple(sorted(node.items())) for node in nodes])]
 
 
 def add_links(relationships):
@@ -31,9 +27,9 @@ def add_links(relationships):
         genre1 = Genre.query.filter_by(genre_id=relationship.genre1_id).first()
         genre2 = Genre.query.filter_by(genre_id=relationship.genre2_id).first()
 
-        links.append({'source': genre1.name,
-                      'target': genre2.name,
-                      'value': relationship.shared_artists})
+        links.append({"source": genre1.name,
+                      "target": genre2.name,
+                      "value": relationship.shared_artists})
 
     # for link in links:
     #     print link
@@ -41,16 +37,102 @@ def add_links(relationships):
     return links
 
 
+def find_genres(feature, limit):
+    """Queries db for all genres that fit feature & limit criteria"""
+
+    f = getattr(GenreAverages, feature)
+
+    genres = set()
+    for genre in GenreAverages.query.filter(f >= limit).all():
+        genres.add(genre.genre)
+
+    return genres
+
+
+def find_relationships(genres, relationships):
+    """Find all relationships for genres in given genre list"""
+
+    results = []
+    for relationship in relationships:
+
+        genre1 = Genre.query.filter_by(genre_id=relationship.genre1_id).first()
+        genre2 = Genre.query.filter_by(genre_id=relationship.genre2_id).first()
+
+        if genre1.name in genres or genre2.name in genres:
+            results.append(relationship)
+
+    nodes = add_nodes(results)
+    links = add_links(results)
+
+    return {"nodes": nodes, "links": links}
+
+
+def find_filename(feature):
+    """Returns the corresponding filename for an audio feature"""
+
+    if feature == 'acousticness':
+        return 'static/genre_maps/acoustic.json'
+    elif feature == 'danceability':
+        return 'static/genre_maps/dancey.json'
+    elif feature == 'energy':
+        return 'static/genre_maps/energetic.json'
+    elif feature == 'instrumentalness':
+        return 'static/genre_maps/instrumental.json'
+    elif feature == 'liveness':
+        return 'static/genre_maps/live.json'
+    elif feature == 'loudness':
+        return 'static/genre_maps/loud.json'
+    elif feature == 'mode':
+        return 'static/genre_maps/mode.json'
+    elif feature == 'speechiness':
+        return 'static/genre_maps/speechy.json'
+    elif feature == 'tempo':
+        return 'static/genre_maps/tempo.json'
+    elif feature == 'valence':
+        return 'static/genre_maps/valence.json'
+
+
+def make_json(feature, limit, relationships):
+    """Given a dictionary of nodes and links, converts to json file"""
+
+    filename = find_filename(feature)
+
+    genres = find_genres(feature, limit)
+
+    results = find_relationships(genres, relationships)
+
+    f = open(filename, 'w')
+
+    json.dump(results, f)
+
 if __name__ == '__main__':
 
     connect_to_db(app)
 
-    relationships = RelatedGenres.query.filter(RelatedGenres.shared_artists > 0).all()
+    r = RelatedGenres.query.filter(RelatedGenres.shared_artists > 0).all()
 
-    nodes = add_nodes(relationships)
-    links = add_links(relationships)
+    # nodes = add_nodes(relationships)
+    # links = add_links(relationships)
 
-    genres = json.dumps({"nodes": nodes,
-                         "links": links})
+    # genres = json.dumps({"nodes": nodes,
+    #                      "links": links})
 
-    print genres
+    # print genres
+
+    make_json('acousticness', 0.5, r, 'static/genre_maps/acoustic.json')
+    make_json('danceability', 0.5, r, 'static/genre_maps/dancey.json')
+    make_json('energy', 0.5, r, 'static/genre_maps/energetic.json')
+    make_json('instrumentalness', 0.5, r, 'static/genre_maps/instrumental.json')
+    make_json('liveness', 0.5, r, 'static/genre_maps/live.json')
+    make_json('loudness', 0.5, r, 'static/genre_maps/loud.json')
+    make_json('mode', 0.5, r, 'static/genre_maps/mode.json')
+    make_json('speechiness', 0.5, r, 'static/genre_maps/speechy.json')
+    make_json('tempo', 0.5, r, 'static/genre_maps/tempo.json')
+    make_json('valence', 0.5, r, 'static/genre_maps/valence.json')
+
+    # feature = 'acousticness'
+    # limit = 0.5
+    # genres = find_genres(feature, limit)
+    # results = find_relationships(genres, relationships)
+    # f = open('static/genre_maps/acoustic.json', 'w')
+    # json.dump(results, f)
