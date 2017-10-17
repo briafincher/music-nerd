5cee7397662c74268e09a7836ce3bddc8bd26c06
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-17 16:44:03 -0700

    Event listeners for genre features

--- static/genre_features.js
+++ static/genre_features.js
@@ -1,37 +1,40 @@
 function categorize(feature, data) {
 
-            var value = data[feature];
+    var value = data[feature];
 
-            var maximum, upper, lower, minimum;
+    var maximum, upper, lower, minimum;
 
-            if (feature === 'loudness') {
-                maximum = 0;
-                upper = -20;
-                lower = -40;
-                minimum = -60;
-            } else if (feature === 'tempo') {
-                maximum = 200;
-                upper = 150;
-                lower = 100;
-                minimum = 50;
-            } else {
-                maximum = 1;
-                upper = 0.66;
-                lower = 0.33;
-                minimum = 0;
-            }
+    if (feature === 'loudness') {
+        maximum = 0;
+        upper = -20;
+        lower = -40;
+        minimum = -60;
+    } else if (feature === 'tempo') {
+        maximum = 200;
+        upper = 150;
+        lower = 100;
+        minimum = 50;
+    } else {
+        maximum = 1;
+        upper = 0.66;
+        lower = 0.33;
+        minimum = 0;
+    }
 
-            console.log(feature)
-            console.log(value)
+    console.log(feature);
+    console.log(value);
 
-            if (value >= upper) {
-                console.log('value')
-                return 'high';
-            } else if (value < upper && value >= lower) {
-                return 'medium';
-            } else {
-                return 'low';
-            }
+    if (value >= upper) {
+        console.log('value')
+        return 'high';
+    } else if (value < upper && value >= lower) {
+        return 'medium';
+    } else {
+        return 'low';
+    }
+}
 
-
+function display(feature) {
+    $('#feature-info').children().hide()
+    $(feature).show()
 }
